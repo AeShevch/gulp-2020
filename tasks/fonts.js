145e6e6b9dedc56d571017d376dfadd76bdda99f
ae.shevch <shevch.ae@gmail.com>
2020-09-08 20:03:01 +0300

    fix fonts

--- tasks/fonts.js
+++ tasks/fonts.js
@@ -1,6 +1,6 @@
 const gulp = require('gulp')
 
 module.exports = function fonts() {
-  return gulp.src('src/fonts/*')
-    .pipe(gulp.dest('dist/fonts'))
+  return gulp.src("src/fonts/*.{eot,ttf,woff,woff2}")
+    .pipe(gulp.dest("dist/fonts"))
 }
