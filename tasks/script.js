12d76dbc31531e13020759f8dda73ddbc538d2a8
ae.shevch <shevch.ae@gmail.com>
2020-09-08 20:01:13 +0300

    adds async-await support

--- tasks/script.js
+++ tasks/script.js
@@ -24,7 +24,15 @@ module.exports = function script() {
               use: {
                 loader: "babel-loader",
                 options: {
-                  presets: ["@babel/preset-env"],
+                  presets: [
+                    ["@babel/preset-env",
+                      {
+                        "useBuiltIns": "usage", // alternative mode: "entry"
+                        "corejs": 3, // default would be 2
+                        "targets": "> 0.25%, not dead"
+                        // set your own target environment here (see Browserslist)
+                      }]
+                  ],
                 },
               },
             },
